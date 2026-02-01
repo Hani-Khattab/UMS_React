0a91b138b0391b369a6c85b788c0a8d4549729cd
Hani-Khattab <hanikhattab266@gmail.com>
2026-02-01 23:06:40 +0200

    Create users page with Loader & error msg

---
+++ src/Components/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Loader() {
+  return(
+
+  <div className="Loader">
+    <span className="loader"></span>
+</div>   
+) 
+
+}
--- src/Pages/Users/Users.jsx
+++ src/Pages/Users/Users.jsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import Loader from '../../Components/Loader/Loader';
 
 export default function Users() {
-  return (
-    <div>Users</div>
+
+  const [users,setUsers] = useState([]);
+  const [isLoader,setIsLoader] = useState(true); 
+  const [error,setIsError] = useState(null); 
+  const getUsers = async ()=>{
+    try{
+
+        const response = await axios.get(`${import.meta.env.VITE_BURL}/users`);
+        console.log(response);
+        setUsers(response.data.users);
+
+    }catch(err){
+    setIsError("Server Error .....")    
+    }
+    finally{
+    setIsLoader(false);
+
+    }
+  }
+  
+  
+useEffect( ()=>{
+
+    getUsers();
+
+} ,[] )
+
+    if(isLoader) return <Loader />
+
+    if(error) return <div className='alert alert-danger'>{error}</div>
+
+    return (
+        <section className='users py-5 text-center'>
+            <div className="container">
+                <div className="row g-3">
+                    {users.map( (user)=>
+                    <div className="col-md-4">
+                        <div className="user bg-success py-3">
+                            <h2>{user.name}</h2>
+                            <img className='Users_Img w-75 ' src={user.imageUrl} alt="Users_Image" />
+                            <p>Email: {user.email}</p>
+                            <span>Age: {user.age}</span>
+
+                        </div>
+                    </div>
+                    )}
+                </div>
+            </div>
+        </section>
   )
 }
